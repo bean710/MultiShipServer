e03d7eb86c7571db2f0410125e03a6846ddeeb19
bean710 <keener4christ@gmail.com>
2016-07-26 14:48:33 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var http = require('http');
+var app = require('express')();
+var io = require('socket.io')(http);
+
+http.Server(app); // eslint-disable-line babel/new-cap
+
+var players = [];
+
+app.get('/', function (req, res) {
+	res.send(players);
+});
+
+io.on('connection', function (socket) {
+	console.log('Player Connected!' + socket.id);
+	socket.emit('socketID', {id: socket.id});
+	socket.emit('getPlayers', players);
+	socket.broadcast.emit('newPlayer', {id: socket.id});
+	socket.on('disconnect', function () {
+		console.log('Player Disconnected');
+		socket.broadcast.emit('playerDisconnected', {id: socket.id});
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].id === socket.id) {
+				players.splice(i, 1);
+			}
+		}
+	});
+	players.push(new Player(socket.id, 0, 0));
+	socket.on('update', function (data) {
+		for (var i = 0; i < players.length; i++) {
+			if (players[i].id === data.id) {
+				players[i].x = data.x;
+				players[i].y = data.y; // socket.broadcast.emit('playerUpdate', data);
+				console.log('Player ' + data.id + ' moved to ' + data.x + ', ' + data.y);
+			}
+		}
+	});
+});
+
+var tick = setInterval(function () {
+	io.emit('fullUpdate', players);
+}, 50);
+console.log(tick);
+
+function Player(id, x, y) {
+	this.id = id;
+	this.x = x;
+	this.y = y;
+}
+
+http.listen(3000, function () {
+	console.log('listening on *:3000');
+});
